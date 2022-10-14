30cf6b017dea2cf679722ac9addc6464e9bc95a8
Marc Plarisan <plarisanmarc@gmail.com>
2022-10-14 19:11:39 +0800

    Added Introduction.java

---
+++ COMPROG1/Prelims/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        final String name = "DragunWF"; // Changed from my real name to username
+        final byte age = 19;
+        final String course = "BSIT";
+        System.out.println(String.format(
+            "Name: %s\nAge: %s\nCourse: %s", name, age, course)
+        );
+    }
+}
