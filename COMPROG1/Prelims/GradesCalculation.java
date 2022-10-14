5af8712b54b35d267dda7eac3ce2e5da1eea4750
Marc Plarisan <plarisanmarc@gmail.com>
2022-10-14 19:11:55 +0800

    Added GradesCalculation.java

---
+++ COMPROG1/Prelims/GradesCalculation.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class GradesCalculation {
+    public static void main(String[] args) {
+        String name = "Marc";
+        System.out.println("Enter your grades:");
+
+        int mathGrade = enterGrade("Math");
+        int scienceGrade = enterGrade("Science");
+        int englishGrade = enterGrade("English");
+        int averageGrade = mathGrade + scienceGrade + englishGrade;
+
+        System.out.println(name + "'s average grade: " + averageGrade);
+    }
+
+    private static int enterGrade(String subjectName) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println(subjectName + ":");
+                int grade = scanner.nextInt();
+                if (!(grade < 0 || grade > 100))
+                    return grade;
+                System.out.println("Grade must be within 0 to 100!");
+            }
+        }
+    }
+}
