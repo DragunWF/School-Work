67e7b0d1ed009ef9799728f7cd6cbe1a777d66fd
Marc Plarisan <plarisanmarc@gmail.com>
2022-10-17 20:00:43 +0800

    Finished activity Pattern.java

--- COMPROG1/Midterms/Pattern.java
+++ COMPROG1/Midterms/Pattern.java
@@ -1,5 +1,38 @@
+import java.util.*;
+
 public class Pattern {
-    public static void main(String[] args) { 
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        final int height = userInput("Enter pattern height:");
+        final int width = userInput("Enter pattern width:");
+        generatePattern(height, width);
+    }
+
+    private static void generatePattern(int height, int width) {
+        boolean rightSnake = true;
         
+        for (int i = 1; i <= height; i++) {
+            String line = "";
+            boolean evenIteration = i % 2 == 0;
+
+            for (int j = 1; j <= width; j++) {
+                if (evenIteration && rightSnake)
+                    line += j == width ? "#" : ".";
+                else if (evenIteration && !rightSnake)
+                    line += j == 1 ? "#" : ".";
+                else
+                    line += "#";
+            }
+
+            if (evenIteration)
+                rightSnake = rightSnake ? false : true;
+            System.out.println(line);
+        }
+    }
+
+    private static int userInput(String prompt) {
+        System.out.println(prompt);
+        return input.nextInt();
     }
 }
