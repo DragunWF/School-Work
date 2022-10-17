25facf5e8dee2357e58aed08b4a31cb9034b2daf
Marc Plarisan <plarisanmarc@gmail.com>
2022-10-17 19:15:15 +0800

    Completed activity AreaOfATriangle.java

---
+++ COMPROG1/Midterms/AreaOfATriangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AreaOfATriangle {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        final double height = userInput("Enter height: ");
+        final double base = userInput("Enter base: ");
+        final double area = (base * height) / 2;
+        System.out.println(String.format("Area of the triangle: %s", area));
+    }
+
+    private static double userInput(String prompt) {
+        while (true) {
+            try {
+                System.out.println(prompt);
+                return input.nextDouble();
+            } catch (Exception error) {
+                System.out.println("Please provide proper input");
+            }
+        }
+    }
+}
