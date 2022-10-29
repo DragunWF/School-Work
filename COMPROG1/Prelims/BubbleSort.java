873d88a2ba5efba0998af2ba56b997eee030b145
Marc Plarisan <plarisanmarc@gmail.com>
2022-10-29 16:46:21 +0800

    Updated BubbleSort.java

--- COMPROG1/Prelims/BubbleSort.java
+++ COMPROG1/Prelims/BubbleSort.java
@@ -1,5 +1,5 @@
 public class BubbleSort {
-    public static void main(String[] args)  { 
+    public static void main(String[] args) {
         int[] unsorted = { -23, 16, 25, 19, 20, 34, 7, 8, 1 };
         int[] sorted = bubbleSort(unsorted.clone());
         displayArr(unsorted, "Unsorted Array");
@@ -7,25 +7,21 @@ public class BubbleSort {
     }
 
     private static void displayArr(int[] arr, String prompt) {
-        System.out.print(prompt + ":");
+        System.out.print(prompt + ": ");
         for (int num : arr)
             System.out.print(num + " ");
-        System.out.print("\n");
+        System.out.println();
     }
 
     private static int[] bubbleSort(int[] arr) {
-        while (true) {
-            boolean unsorted = false;
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    int tempNum = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = tempNum;
-                    unsorted = true;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-            if (!unsorted)
-                break;
         }
         return arr;
     }
