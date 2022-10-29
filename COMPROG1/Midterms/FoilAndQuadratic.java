627f539c7f1f5d8d0707c384851c61a67a4de041
Marc Plarisan <plarisanmarc@gmail.com>
2022-10-29 16:33:07 +0800

    Code cleanups

--- COMPROG1/Midterms/FoilAndQuadratic.java
+++ COMPROG1/Midterms/FoilAndQuadratic.java
@@ -1,5 +1,5 @@
 public class FoilAndQuadratic {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         final String wall = "----------------------------------";
         System.out.println(wall);
 
@@ -8,7 +8,7 @@ public class FoilAndQuadratic {
 
         System.out.println();
 
-        System.out.println("Second problem: (Foil Method");
+        System.out.println("Second problem: (Foil Method)");
         solveSecond();
 
         System.out.println(wall);
@@ -28,42 +28,42 @@ public class FoilAndQuadratic {
         System.out.println(String.format("Answer: %s", ans));
     }
 
+    private static void printProblem(String problem) {
+        System.out.println(String.format("Solve: %s", problem));
+    }
+
     private static void solveFirst() {
-        System.out.println("Solve: (5n + 3)(n + 6)");
-        final int a, b, c, d;
-        a = 5; b = 3; c = 4; d = 6;
-        final int x = b * d + (c - c);
-        final int y = a * d + b;
+        printProblem("(5n + 3)(n + 6)");
+        final int a = 5, b = 3, c = 6;
+        final int x = b * c;
+        final int y = a * c + b;
         printAnswer(String.format("%sn^2 + %sn + %s", a, y, x));
     }
 
     private static void solveSecond() {
-        System.out.println("Solve: (x + 1)(2x - 3)");
-        final int a, b, c, d;
-        a = 1; b = 1; c = 2; d = -3;
+        printProblem("(x + 1)(2x - 3)");
+        final int a = 1, b = 1, c = 2, d = -3;
         final int x = a * c;
         final int y = Math.abs(b * d); // Abs just for the output and display
         printAnswer(String.format("%sx^2 - x - %s", x, y));
     }
 
     private static void solveThird() {
-        System.out.println("Solve: 2x^2 + x - 300 = 0");
-        final int a, b, c;
-        a = 2; b = 1; c = -300;
-        final double x = Math.sqrt(b * b - 4 * (2 * c));
+        printProblem("2x^2 + x - 300 = 0");
+        final int a = 2, b = 1, c = -300;
+        final double x = Math.sqrt(b * b - 4 * (a * c));
         final double y = (-b + x) / (a * a);
         final double z = (-b - x) / (a * a);
-        printAnswer(String.format("x = %s, x = %s", (int)y, z));
-    
+        printAnswer(String.format("x = %s, x = %s", (int) y, z));
+
     }
 
     private static void solveFourth() {
-        System.out.println("Solve: x^2 + 4x - 5 = 0");
-        final double a, b, c;
-        a = 1; b = 4; c = -5;
-        final double x = Math.sqrt(4 * 4 - 4 * a * c);
+        printProblem("x^2 + 4x - 5 = 0");
+        final double a = 1, b = 4, c = -5;
+        final double x = Math.sqrt(b * b - b * a * c);
         final double y = (-b + x) / (2 * c);
         final double z = (-b - x) / (2 * c);
-        printAnswer(String.format("x = %s, x = %s", (int)y, (int)z));
+        printAnswer(String.format("x = %s, x = %s", (int) y, (int) z));
     }
 }
