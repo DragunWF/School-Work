2b2495417a2b99fc2379c9fd12806d162f2bee07
Marc Plarisan <plarisanmarc@gmail.com>
2022-11-03 15:24:28 +0800

    Completed Challenge.java

---
+++ COMPROG1/Midterms/Challenge.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Challenge {
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 Numbers:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(String.format("Index %s: ", i));
+            arr[i] = input.nextInt();
+        }
+
+        reverse(arr);
+    }
+
+    private static void reverse(int[] arr) {
+        System.out.println("\nReversed:");
+        for (int i = arr.length - 1; i >= 0; i--)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
