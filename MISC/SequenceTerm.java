d8f32b2428f05f6b4124743a302d312e5bb3956b
Marc Plarisan <plarisanmarc@gmail.com>
2022-11-10 21:06:39 +0800

    Created SequenceTerm.java for reviewing MMW

---
+++ MISC/SequenceTerm.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class SequenceTerm {
+    private static final Scanner input = new Scanner(System.in);
+    private static final int d = 4;
+    private static final int a = 1;
+
+    public static void main(String[] args) {
+        System.out.printf("d = %s, a = %s\n", d, a);
+        final int term = userInput("Enter the nth term: ");
+        final int userAns = userInput("Enter your answer: ");
+        final int correctAns = a + (term - 1) * d;
+
+        if (userAns == correctAns) {
+            System.out.println("Congrats you got it right!");
+        } else {
+            System.out.printf("Wrong! The correct answer was %s!\n", correctAns);
+        }
+    }
+
+    private static int userInput(String prompt) {
+        System.out.print(prompt);
+        return input.nextInt();
+    }
+}
