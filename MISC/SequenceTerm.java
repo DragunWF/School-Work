d9aa14efeff0ed5bb4e5601ef88bf1a11035e556
Marc Plarisan <plarisanmarc@gmail.com>
2022-11-10 21:17:15 +0800

    Changed term variable name to n in SequenceTerm.java reviewer

--- MISC/SequenceTerm.java
+++ MISC/SequenceTerm.java
@@ -7,9 +7,9 @@ public class SequenceTerm {
 
     public static void main(String[] args) {
         System.out.printf("d = %s, a = %s\n", d, a);
-        final int term = userInput("Enter the nth term: ");
+        final int n = userInput("Enter the nth term: ");
         final int userAns = userInput("Enter your answer: ");
-        final int correctAns = a + (term - 1) * d;
+        final int correctAns = a + (n - 1) * d;
 
         if (userAns == correctAns) {
             System.out.println("Congrats you got it right!");
