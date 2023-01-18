1b1e57fb0952e5533ec39957d28fdcef429e90a0
Marc Plarisan <plarisanmarc@gmail.com>
2023-01-19 02:44:36 +0800

    Updated MatrixArray.java Cleaned up code

--- COMPROG1/Finals/MatrixArray.java
+++ COMPROG1/Finals/MatrixArray.java
@@ -1,5 +1,6 @@
 public class MatrixArray {
-    private static int[][] matrix = new int[9][9];
+    private static final int size = 9;
+    private static int[][] matrix = new int[size][size];
     private static char[] alphabets = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 
     public static void main(String[] args) {
@@ -11,8 +12,8 @@ public class MatrixArray {
     }
 
     private static void randomizeMatrix() {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 int random = (int) Math.floor(Math.random() * 10);
                 matrix[i][j] = random;
             }
@@ -21,16 +22,16 @@ public class MatrixArray {
 
     private static void displayAlphabets() {
         System.out.print("    ");
-        for (int i = 0; i < matrix.length; i++) {
+        for (int i = 0; i < size; i++) {
             System.out.printf("%s ", alphabets[i]);
         }
         System.out.println();
     }
 
     private static void displayMatrix() {
-        for (int i = 0; i < matrix.length; i++) {
+        for (int i = 0; i < size; i++) {
             System.out.printf("%s | ", i + 1);
-            for (int j = 0; j < matrix.length; j++) {
+            for (int j = 0; j < size; j++) {
                 System.out.printf("%s ", matrix[i][j]);
             }
             System.out.println();
