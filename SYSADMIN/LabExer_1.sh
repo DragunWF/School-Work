f69bb0b67605a998914789a08fb5ddab9e73310c
Marc Plarisan <plarisanmarc@gmail.com>
2023-05-29 10:24:54 +0800

    Create LabExer_1.sh

---
+++ SYSADMIN/LabExer_1.sh
@@ -0,0 +1,9 @@
+# This is my first script
+clear
+echo "I am Marc"
+echo "I am 20 years old"
+echo "My full name is Marc [REDACTED]"
+echo "My motto in life is to improve my programming skills"
+echo "_"
+echo "My goal is to graduate from this school"
+# echo "Hidden from timeline!"
