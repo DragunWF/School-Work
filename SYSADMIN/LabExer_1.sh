bba0baef4435e0793fd2e64eb4908578854b45db
Marc Plarisan <plarisanmarc@gmail.com>
2023-05-29 10:30:43 +0800

    Update LabExer_1.sh

--- SYSADMIN/LabExer_1.sh
+++ SYSADMIN/LabExer_1.sh
@@ -3,7 +3,7 @@ clear
 echo "I am Marc"
 echo "I am 20 years old"
 echo "My full name is Marc [REDACTED]"
-echo "My motto in life is to improve my programming skills"
+echo "My motto in life is to improve my programming skills and achieve success"
 echo "_"
 echo "My goal is to graduate from this school"
 # echo "Hidden from timeline!"
