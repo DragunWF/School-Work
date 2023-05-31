bb1e046e1e6f7960c45d103e784658d3f1e1e1b0
Marc Plarisan <plarisanmarc@gmail.com>
2023-05-31 13:47:07 +0800

    Update TaskPerformance.sh

--- SYSADMIN/TaskPerformance.sh
+++ SYSADMIN/TaskPerformance.sh
@@ -1,5 +1,13 @@
 echo "Type a name for your new directory"
 read dirName
 mkdir $dirName
+
+echo "Type another name for your new sub directory"
+read subDirName
+mkdir "$dirName/$subDirName"
+
 touch "grades"
 chmod 755 grades
+
+sudo apt install tree
+tree
