f32960e8bc730191b9857c293c0f64bffb8c22d9
Marc Plarisan <plarisanmarc@gmail.com>
2023-05-31 13:54:23 +0800

    Update LabExer_1.sh

--- SYSADMIN/LabExer_1.sh
+++ SYSADMIN/LabExer_1.sh
@@ -4,6 +4,7 @@ clear
 echo "I am Marc"
 echo "I am 20 years old"
 echo "My full name is Marc [REDACTED]"
+echo "From BSIT - 205"
 echo "My motto in life is to improve my programming skills and achieve success"
 echo "_"
 echo "My goal is to graduate from this school"
