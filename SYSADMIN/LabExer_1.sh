cfe3dff0d30c4bcf5067ffe7e18576ff4a6915b6
dragunwf <plarisanmarc@gmail.com>
2023-06-02 11:31:48 +0800

    Update LabExer_1.sh

--- SYSADMIN/LabExer_1.sh
+++ SYSADMIN/LabExer_1.sh
@@ -2,10 +2,21 @@
 # Not actually the first but the instructions require me to type this
 clear
 echo "I am Marc"
-echo "I am 20 years old"
+echo "I am [REDACTED] years old"
 echo "My full name is Marc [REDACTED]"
 echo "From BSIT - 205"
 echo "My motto in life is to improve my programming skills and achieve success"
 echo "_"
 echo "My goal is to graduate from this school"
 # echo "Hidden from timeline!"
+
+# ------- Separate Script Below -------
+
+# Script to print user info, current date & time
+clear
+echo "Hello "
+whoami
+echo "Today is `date`"
+echo "Number of user login: `who | wc -l`"
+echo "    Calendar"
+cal
