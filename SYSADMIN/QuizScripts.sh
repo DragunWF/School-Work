d95f43d954216fdf198332aae1b43baf837c3a56
dragunwf <plarisanmarc@gmail.com>
2023-06-05 19:48:22 +0800

    Create QuizScripts.sh

---
+++ SYSADMIN/QuizScripts.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Script no.1
+# This is a comment!
+echo "Aim high, STI!"
+echo "This is a comment!"
+echo This is a comment
+
+# Script no.2
+word1=Hello
+word2=How
+word3=you
+word4=are
+echo "$word2 $word4 $word2? $word1?!?"
+
+# Script no.3
+Name1=John
+echo "Who are you?"
+Name2=Micheal
+echo "Nice to meet you $Name1"
+
+# Script no.4
+aa=4
+ab=5
+ac=6
+let "aa = aa + ac"
+echo "The first operation is equal to $aa"
+let "ac = ac - ab"
+echo "The second operation is equal to $ac"
