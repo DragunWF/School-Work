ead2459f458b3916ca5869cb057721b119e623ce
Marc Plarisan <plarisanmarc@gmail.com>
2023-06-23 21:03:21 +0800

    Update PracticeConditionals.sh

--- misc/Other/Bash/PracticeConditionals.sh
+++ misc/Other/Bash/PracticeConditionals.sh
@@ -7,3 +7,25 @@ if [ $input == "yes" ]; then
 else
     echo "Okay"
 fi
+
+echo "Now type in a number"
+read num
+if [ $num -ge 5 ]; then
+    echo "Number is greater or equal to 5"
+else
+    echo "Number is not greater or equal to 5"
+fi
+
+echo "Now type in a letter"
+read letter
+case $letter in
+    a) 
+        echo "You typed the letter a"
+        ;;
+    b)
+        echo "You typed the letter b"
+        ;;
+    *)
+        echo "You did not type either the letter a or b"
+        ;;
+esac
