cdffeff99101773d3dd548606dd08d66e7c0d883
dragunwf <plarisanmarc@gmail.com>
2023-06-25 21:55:11 +0800

    Update App.java

--- misc/MatrixOperations/src/App.java
+++ misc/MatrixOperations/src/App.java
@@ -1,16 +1,26 @@
 import java.util.*;
 
 public class App {
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        Matrix a = new Matrix(3, 3);
-        Matrix b = new Matrix(3, 3);
+        Matrix a = new Matrix(2, 3);
+        Matrix b = new Matrix(2, 3);
 
-        a.display("a");
         System.out.println();
-        b.display("b");
-        System.out.println();
+        show(a, "a");
+        show(b, "b");
 
-        a.add(b);
-        a.display("a + b");
+        show(a.add(b), "r");
+        show(a.transpose(), "a^t");
+    }
+
+    private static void show(Matrix matrix, String varName) {
+        if (matrix != null) {
+            matrix.display(varName);
+        } else {
+            System.out.println("undefined");
+        }
+        System.out.println();
     }
 }
