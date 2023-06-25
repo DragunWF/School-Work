bebc6c3f2b690b3d59833f046c813fc99378dab8
dragunwf <plarisanmarc@gmail.com>
2023-06-25 22:48:16 +0800

    Update App.java

--- misc/MatrixOperations/src/App.java
+++ misc/MatrixOperations/src/App.java
@@ -5,13 +5,15 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Matrix a = new Matrix(2, 3);
-        Matrix b = new Matrix(2, 3);
+        Matrix b = new Matrix(3, 2);
 
         System.out.println();
         show(a, "a");
         show(b, "b");
 
-        show(a.add(b), "r");
+        show(a.add(b), "a + b");
+        show(a.subtract(b), "a - b");
+        show(a.multiply(b), "a * b");
         show(a.transpose(), "a^t");
     }
 
