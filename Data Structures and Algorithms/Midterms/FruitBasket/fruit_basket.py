9623175e8238a7960af7c6e40b94b803579c8dfe
dragunwf <plarisanmarc@gmail.com>
2023-10-09 14:29:22 +0800

    Update fruit_basket.py

--- Data Structures and Algorithms/Midterms/FruitBasket/fruit_basket.py
+++ Data Structures and Algorithms/Midterms/FruitBasket/fruit_basket.py
@@ -1,24 +1,24 @@
 basket = []
 
 
-def display_basket() -> None:
-    for fruit in basket:
-        print(f"| {fruit} |")
-
-
 def lunch_time() -> None:
+    print(f"Your basket now has: {basket}")
+    initial_len = len(basket)
     while len(basket) > 0:
-        option = input().upper()
+        if len(basket) < initial_len:
+            print(f"Fruit(s) in the basket: {basket}")
+        option = input("Press E to eat a fruit ").upper()
         if option == "E":
             basket.pop()
-        display_basket()
     print("No more fruits")
 
 
 def main() -> None:
-    fruit_amount = int(input("Enter your desired fruit amount: "))
+    print("Catch and eat any of these fruits:('apple', 'orange', 'mango', 'guava')")
+    fruit_amount = int(input("How many fruits would you like to catch? "))
+    print("Choose a fruit to catch. Press A, O, M, or G.")
     for i in range(fruit_amount):
-        fruit = input(f"Fruit {i + 1}: ")
+        fruit = input(f"Fruit {i + 1} of {fruit_amount}: ")
         match fruit.upper():
             case "A": basket.append("Apple")
             case "O": basket.append("Orange")
