a99dfe07aeabdf6c59f57c3b8a75ceb7a2af34b8
Marc Plarisan <plarisanmarc@gmail.com>
2023-10-23 11:16:43 +0800

    Create HtmlTree.java

---
+++ Data Structures and Algorithms/Midterms/HtmlTree/HtmlTree.java
@@ -0,0 +1,10 @@
+import javax.swing;
+import javax.swing.tree;
+
+public class HtmlTree {
+    public static void main(String[] args) {
+        JTree tree = new JTree(
+            
+        );
+    }
+}
