12834fa6631644a096c42eb4b985ce225c98857e
dragunwf <plarisanmarc@gmail.com>
2023-11-28 16:50:35 +0800

    Update fcfs.py

--- Miscelleneous/Scheduling/fcfs.py
+++ Miscelleneous/Scheduling/fcfs.py
@@ -4,7 +4,7 @@ from collections import deque
 
 
 class Process:
-    def __init__(self, name:str, arrival_time:int, burst_time: int):
+    def __init__(self, name: str, arrival_time: int, burst_time: int):
         self.__name = name
         self.__arrival_time = arrival_time
         self.__burst_time = burst_time
@@ -13,21 +13,23 @@ class Process:
 
     def calculate_turnaround_time(self) -> None:
         if self.__waiting_time == None:
-            print(f"Waiting time has to be calculated first for process {self.__name}")
+            print(
+                f"Waiting time has to be calculated first for process {self.__name}")
         elif self.__turnaround_time != None:
             self.__turnaround_time = self.__burst_time + self.__waiting_time
         else:
-            print(f"Turnaround time has already been calculated for process {self.__name}")
+            print(
+                f"Turnaround time has already been calculated for process {self.__name}")
 
     def get_name(self) -> str:
         return self.__name
-    
+
     def get_arrival_time(self) -> int:
         return self.__arrival_time
-    
+
     def get_burst_time(self) -> int:
         return self.__burst_time
-    
+
     def get_waiting_time(self) -> int:
         return self.__waiting_time
 
@@ -37,41 +39,48 @@ class Process:
 
 class FCFS:
     def __init__(self, attributes: tuple[tuple[int]]):
-        self.__ready_queue:list[Process] = deque([])
+        self.__attributes: tuple[tuple[int]] = attributes
+        self.__ready_queue: list[Process] = deque([])
         self.__average_waiting_time: int = None
         self.__average_turnaround_time: int = None
-        self.__burst_times:list[tuple[int]] = []
+        self.__burst_times: list[tuple[int]] = []
 
     def main(self) -> None:
-        self.add_table_contents(self.__ready_queue)
-        self.bubble_sort(self.__ready_queue)
+        self.add_table_contents()
+        self.bubble_sort()
         Test.print_values(self.__ready_queue, "arrival_time")
-        self.display_processes(self.__ready_queue)
+        self.display_processes()
+        self.display_gantt_chart()
 
-    def add_table_contents(self, queue: list[Process]) -> None:
-        # First index is arrival time and the second index is burst time
-        attributes:tuple[tuple[int]] = ((2, 3), (3, 5),(0, 2), (1, 4), (4, 6))
-        for i in range(len(attributes)):
-            queue.append(Process(f"P{i + 1}", attributes[i][0], attributes[i][1]))
+    def add_table_contents(self) -> None:
+        for i in range(len(self.__attributes)):
+            self.__ready_queue.append(
+                Process(f"P{i + 1}", self.__attributes[i][0], self.__attributes[i][1]))
 
-    def display_processes(self, queue: list[Process]) -> None:
-        current_waiting_time:int = 0
-        for process in queue:
-            self.__burst_times.append((current_waiting_time, current_waiting_time + process.get_burst_time()))
+    def display_processes(self) -> None:
+        current_waiting_time: int = 0
+        for process in self.__ready_queue:
+            self.__burst_times.append(
+                (current_waiting_time, current_waiting_time + process.get_burst_time()))
             current_waiting_time += process.get_burst_time()
 
-    def bubble_sort(self, queue: list[Process]) -> None:
-        for i in range(len(queue)):
-            is_sorted:bool = True
-            for j in range(len(queue) - 1):
-                if queue[j].get_arrival_time() > queue[j + 1].get_arrival_time():
-                    queue[j], queue[j + 1] = queue[j + 1], queue[j]
+    def bubble_sort(self) -> None:
+        for i in range(len(self.__ready_queue)):
+            is_sorted: bool = True
+            for j in range(len(self.__ready_queue) - 1):
+                if self.__ready_queue[j].get_arrival_time() > self.__ready_queue[j + 1].get_arrival_time():
+                    self.__ready_queue[j], self.__ready_queue[j + 1] = (
+                        self.__ready_queue[j + 1], self.__ready_queue[j])
                     is_sorted = False
             if is_sorted:
                 break
 
-    def display_gantt_chart(self) -> None:
+    def calculate_waiting_times(self):
         pass
+
+    def display_gantt_chart(self) -> None:
+        print(Test.print_values(self.__ready_queue, "name"))
+        print(self.__burst_times)
 
 
 class Test:
@@ -89,4 +98,5 @@ class Test:
 
 
 if __name__ == '__main__':
-    FCFS().main()
+    # First index is the arrival time and the second index is the burst time
+    FCFS(((2, 3), (3, 5), (0, 2), (1, 4), (4, 6))).main()
