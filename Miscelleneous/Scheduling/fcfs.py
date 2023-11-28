9058ba972d2adc92c09eac9bf236d85f1cbc097c
dragunwf <plarisanmarc@gmail.com>
2023-11-28 18:59:01 +0800

    Update fcfs.py

--- Miscelleneous/Scheduling/fcfs.py
+++ Miscelleneous/Scheduling/fcfs.py
@@ -11,16 +11,6 @@ class Process:
         self.__waiting_time = None
         self.__turnaround_time = None
 
-    def calculate_turnaround_time(self) -> None:
-        if self.__waiting_time == None:
-            print(
-                f"Waiting time has to be calculated first for process {self.__name}")
-        elif self.__turnaround_time != None:
-            self.__turnaround_time = self.__burst_time + self.__waiting_time
-        else:
-            print(
-                f"Turnaround time has already been calculated for process {self.__name}")
-
     def get_name(self) -> str:
         return self.__name
 
@@ -30,11 +20,24 @@ class Process:
     def get_burst_time(self) -> int:
         return self.__burst_time
 
-    def get_waiting_time(self) -> int:
-        return self.__waiting_time
+    def calculate_turnaround_time(self) -> None:
+        if self.__waiting_time is None:
+            print(
+                f"Waiting time has to be calculated first for process {self.__name}")
+        elif self.__turnaround_time is None:
+            self.__turnaround_time = self.__burst_time + self.__waiting_time
+        else:
+            print(
+                f"Turnaround time has already been calculated for process {self.__name}")
 
     def get_turnaround_time(self) -> int:
         return self.__turnaround_time
+
+    def set_waiting_time(self, value: int) -> None:
+        self.__waiting_time = value
+
+    def get_waiting_time(self) -> int:
+        return self.__waiting_time
 
 
 class FCFS:
@@ -48,16 +51,23 @@ class FCFS:
     def main(self) -> None:
         self.add_table_contents()
         self.bubble_sort()
-        Test.print_values(self.__ready_queue, "arrival_time")
-        self.display_processes()
+        self.order_processes()
+        self.calculate_waiting_times()
+        self.calculate_turnaround_times()
+        self.calculate_averages()
+
+        print("Gantt Chart")
         self.display_gantt_chart()
+
+        print(f"Average Waiting Time: {self.__average_waiting_time}")
+        print(f"Average Turnaround Time: {self.__average_turnaround_time}")
 
     def add_table_contents(self) -> None:
         for i in range(len(self.__attributes)):
             self.__ready_queue.append(
                 Process(f"P{i + 1}", self.__attributes[i][0], self.__attributes[i][1]))
 
-    def display_processes(self) -> None:
+    def order_processes(self) -> None:
         current_waiting_time: int = 0
         for process in self.__ready_queue:
             self.__burst_times.append(
@@ -66,7 +76,7 @@ class FCFS:
 
     def bubble_sort(self) -> None:
         for i in range(len(self.__ready_queue)):
-            is_sorted: bool = True
+            is_sorted = True
             for j in range(len(self.__ready_queue) - 1):
                 if self.__ready_queue[j].get_arrival_time() > self.__ready_queue[j + 1].get_arrival_time():
                     self.__ready_queue[j], self.__ready_queue[j + 1] = (
@@ -76,11 +86,26 @@ class FCFS:
                 break
 
     def calculate_waiting_times(self):
-        pass
+        for i in range(len(self.__ready_queue)):
+            self.__ready_queue[i].set_waiting_time(self.__burst_times[i][0])
+
+    def calculate_turnaround_times(self):
+        for i in range(len(self.__ready_queue)):
+            self.__ready_queue[i].calculate_turnaround_time()
+
+    def calculate_averages(self):
+        queue_len = len(self.__ready_queue)
+        self.__average_waiting_time = round(
+            sum([p.get_waiting_time() for p in self.__ready_queue]) / queue_len, 4)
+        self.__average_turnaround_time = round(
+            sum([p.get_turnaround_time() for p in self.__ready_queue]) / queue_len, 4)
 
     def display_gantt_chart(self) -> None:
-        print(Test.print_values(self.__ready_queue, "name"))
-        print(self.__burst_times)
+        queue_len = len(self.__ready_queue)
+        for i in range(queue_len):
+            process = self.__ready_queue[i]
+            print(f"{process.get_waiting_time()} {process.get_name()}", end=" ")
+        print(self.__burst_times[queue_len - 1][1])
 
 
 class Test:
@@ -88,11 +113,11 @@ class Test:
     def print_values(values: list[Process], key: str) -> None:
         match (key):
             case "arrival_time":
-                print([process.get_arrival_time() for process in values])
+                print([p.get_arrival_time() for p in values])
             case "burst_time":
-                print([process.get_burst_time() for process in values])
+                print([p.get_burst_time() for p in values])
             case "name":
-                print([process.get_name() for process in values])
+                print([p.get_name() for p in values])
             case _:
                 raise Exception("Invalid key for process!")
 
