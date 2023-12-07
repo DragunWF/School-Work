1dfe3a47f3f3a6338021ea85856653ada31806ec
Marc Plarisan <plarisanmarc@gmail.com>
2023-12-07 11:39:42 +0800

    Update playground.py

--- Miscelleneous/Scheduling/playground.py
+++ Miscelleneous/Scheduling/playground.py
@@ -1,7 +1,10 @@
 from rich import print
 from collections import deque
 
-# Testing for the Round Robin scheduling algorithm
+
+class Item:
+    def __init__(self, value: int):
+        self.value = value
 
 def main() -> None:
     items = ["Scientist", "Programmer", "Lawyer"]
@@ -23,8 +26,20 @@ def foo() -> None:
     while values:
         print(values[0])
         values.popleft()
+
+
+def bar() -> None:
+    values = [Item(3), Item(4), Item(5)]
+    second = []
+    item = values[0]
+    print(item.value)
+    second.append(values[0])
+    item.value = 7
+    print(values[0].value)
+    print(second[0].value)
     
 
 if __name__ == "__main__":
     # main()
-    foo()
+    # foo()
+    bar()
