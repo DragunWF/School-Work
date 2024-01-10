cfda33ffbdd5e36e4311b1680ae46333c7066e6d
dragunwf <plarisanmarc@gmail.com>
2024-01-10 12:30:22 +0800

    Update task_performance.py Formatting

--- Data Structures and Algorithms/Finals/task_performance.py
+++ Data Structures and Algorithms/Finals/task_performance.py
@@ -32,7 +32,7 @@ class DialogueTree:
 
     def end(self) -> None:
         while True:
-            option = Utils.input("Is there anything else you need help with? " + 
+            option = Utils.input("Is there anything else you need help with? " +
                                  "Please answer with a simple yes or no")
             sleep(2)
             if "yes" in option:
@@ -60,7 +60,7 @@ class Utils:
 class Dialogue:
     @staticmethod
     def intro() -> str:
-        words = Utils.input("May we help you with our store's categories or " + 
+        words = Utils.input("May we help you with our store's categories or " +
                             "do you need to meet with the manager?")
         sleep(2)
         if Utils.contains(words, "category", "categories", "sections"):
@@ -68,41 +68,41 @@ class Dialogue:
         elif Utils.contains(words, "manager", "managers", "boss", "master"):
             return "right"
         sleep(2)
-        print("I'm sorry but I don't comprehend your request. " + 
+        print("I'm sorry but I don't comprehend your request. " +
               "Do you need help with categories or the manager?")
         sleep(1)
         return Dialogue.intro()
 
     @staticmethod
     def categories() -> None:
-        print("The categories available are food, utilities, " + 
+        print("The categories available are food, utilities, " +
               "school supplies, and medicine...")
         sleep(2)
 
         words = Utils.input("Which one would you like to know about?")
         sleep(2)
         if "food" in words:
-            print("It is located at section A1 of the building. " + 
+            print("It is located at section A1 of the building. " +
                   "The section contains meat, diary products, and junk food.")
         elif Utils.contains(words, "utility", "utilities"):
             print("The utility section is located at section A2 of the building")
         elif Utils.contains(words, "school", "supplies", "college", "schools", "university"):
-            print("If you're looking for school supplies, it is located at " + 
+            print("If you're looking for school supplies, it is located at " +
                   "section A3 of the building")
         elif Utils.contains(words, "medical", "medicine", "pills", "bandages", "bandage", "aid"):
-            print("If you are looking for medicine, you should go to section B4 of " + 
+            print("If you are looking for medicine, you should go to section B4 of " +
                   "the building which is all the way at one of the corners.")
         else:
-            print("I do not comprehend what your request, " + 
+            print("I do not comprehend what your request, " +
                   "which category are you looking for exactly?")
             sleep(1)
-            print("The category you are looking for may not exist, " + 
+            print("The category you are looking for may not exist, " +
                   "please choose the categories I listed previously...")
             sleep(1)
             return Dialogue.categories()
 
         while True:
-            words = Utils.input("Is there anything else you need help with the categories? " + 
+            words = Utils.input("Is there anything else you need help with the categories? " +
                                 "Answer with either yes or no")
             sleep(1)
             if Utils.contains(words, "yes", "absolutely"):
@@ -111,24 +111,24 @@ class Dialogue:
             elif Utils.contains(words, "no", "not"):
                 break
             else:
-                print("Please answer with either yes or no... " + 
+                print("Please answer with either yes or no... " +
                       "Or you can answer with something like 'absolutely'")
                 sleep(1)
 
     @staticmethod
     def manager(repeat=False) -> str:
         if not repeat:
-            print("Do you a problem with our store that you " + 
+            print("Do you a problem with our store that you " +
                   "would like to report to the manager?")
         sleep(3)
-        words = Utils.input("If so, is it with the employees or " + 
+        words = Utils.input("If so, is it with the employees or " +
                             "the environment of the store?")
         sleep(2)
         if Utils.contains(words, "no", "not", "none", "don't", "dont"):
-            print("I see, well you can only contact the manager " + 
+            print("I see, well you can only contact the manager " +
                   "with problems about the store")
             sleep(2)
-            print("But since you don't have any problems, " + 
+            print("But since you don't have any problems, " +
                   "let's talk about something else...")
             sleep(2)
             return
@@ -137,7 +137,7 @@ class Dialogue:
         elif Utils.contains(words, "environment", "surroundings", "enviroments"):
             return "right"
         else:
-            print("I do not comprehend your message, " + 
+            print("I do not comprehend your message, " +
                   "is it with the employees or the environment")
             Dialogue.manager(True)
         sleep(2)
@@ -146,13 +146,13 @@ class Dialogue:
     def employees(visited=False) -> None:
         if visited:
             sleep(1)
-            print("You have already reported issues about our employees and workers. " + 
+            print("You have already reported issues about our employees and workers. " +
                   "Please try again in our next session.")
             sleep(2)
             return
         Utils.input("What are your problems about our employees?")
         sleep(3)
-        print("I see... Email us a full report for consideration. " + 
+        print("I see... Email us a full report for consideration. " +
               "We have placed you on the meeting queue.")
         sleep(1)
 
@@ -160,14 +160,14 @@ class Dialogue:
     def environment(visited=False) -> None:
         if visited:
             sleep(1)
-            print("You have already reported problems about our store's environment. " + 
+            print("You have already reported problems about our store's environment. " +
                   "Please try again in our next interaction.")
             sleep(1)
             return
-        Utils.input("What are your issues about our " + 
+        Utils.input("What are your issues about our " +
                     "store's environment and surroundings?")
         sleep(3)
-        print("Affirmative, please email us a full report for consideration. " + 
+        print("Affirmative, please email us a full report for consideration. " +
               "We have now added you to the meeting queue.")
         sleep(1)
 
