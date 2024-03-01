da078ad568a7b207b3e26c3833ac937147a9ed21
Marc Plarisan <plarisanmarc2@gmail.com>
2024-03-01 14:13:36 +0800

    Update GreetingProgram.cs

--- Integrative Programming/Prelims/GreetingApp/GreetingProgram.cs
+++ Integrative Programming/Prelims/GreetingApp/GreetingProgram.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 
-namespace GreetingApp {
+namespace GreetingApp
+{
     class GreetingProgram
     {
-        public static void Main()
+        static void Main()
         {
             string name = input("Enter your name");
             int enrolledCourses = int.Parse(input("Enter the total number of your enrolled courses"));
             float favoriteBookPrice = float.Parse(input("Enter the price of your favorite book"));
             Console.WriteLine($"Name: {name}");
-            Console.WriteLine($"Enrolled Courses: {enrolledCourses}");
-            Console.WriteLine($"Favorite Book Price: {favoriteBookPrice}");
+            Console.WriteLine($"Total enrolled courses: {enrolledCourses}");
+            Console.WriteLine($"Price of my favorite book: {favoriteBookPrice}");
         }
 
         private static string input(string prompt)
