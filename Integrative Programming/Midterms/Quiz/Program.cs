cd5ca2e95865e1ca5e5a985901ecc91d47b19ce3
dragunwf <plarisanmarc2@gmail.com>
2024-04-02 07:35:07 +0800

    Finish Quiz

---
+++ Integrative Programming/Midterms/Quiz/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Doing this for practice
+        // 1
+        string[] classmates = { "Bastian", "Sardia", "Buante", "Gidan", "Kyle", "Marc" };
+        foreach (string classmate in classmates)
+        {
+            Console.WriteLine(classmate);
+        }
+
+        // 2
+        char[][] alphabets = {
+            new char[] { 'a', 'b', 'c' }, new char[] { 'd', 'e', 'f' }
+        };
+
+        // 3
+        string message = "why hello there!";
+        Console.WriteLine(message.Contains("hello"));
+    }
+}
