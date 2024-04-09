8c390f1a5904bcc9f47812886bb504260416fee2
dragunwf <plarisanmarc2@gmail.com>
2024-04-09 19:17:51 +0800

    Finish Lab Exercise Was completely optional to do because im exemted from the midterms and prefinals but i still wanted to do it for practice

---
+++ Integrative Programming/Midterms/StudentRegistrationForm/README.md
@@ -0,0 +1,5 @@
+# Student Registration Form
+
+## Image of the App
+
+![Image of the App](Images/Output.JPG)
--- Integrative Programming/Midterms/StudentRegistrationForm/StudentRegistrationForm/Form1.cs
+++ Integrative Programming/Midterms/StudentRegistrationForm/StudentRegistrationForm/Form1.cs
@@ -5,6 +5,58 @@ namespace StudentRegistrationForm
         public Form1()
         {
             InitializeComponent();
+            List<string> programs = new()
+            {
+                "Bachelor of Science in Computer Science",
+                "Bachelor of Science in Information Technology",
+                "Bachelor of Science in Information Science",
+                "Bachelor of Science in Computer Engineering"
+            };
+
+            for (int i = 1; i <= 31; i++)
+            {
+                dayComboBox.Items.Add(i.ToString());
+            }
+            for (int i = 1; i <= 12; i++)
+            {
+                monthComboBox.Items.Add(i.ToString());
+            }
+            for (int i = 1900; i <= 2024; i++)
+            {
+                yearComboBox.Items.Add(i.ToString());
+            }
+            foreach (string program in programs)
+            {
+                programComboBox.Items.Add(program);
+            }
+        }
+
+        private void OnRegisterStudent(object sender, EventArgs e)
+        {
+            if (firstNameText.Text.Length == 0 || lastNameText.Text.Length == 0)
+            {
+                MessageBox.Show("First and last name fields cannot be empty!");
+            }
+            else if (!maleRadioButton.Checked && !femaleRadioButton.Checked)
+            {
+                MessageBox.Show("Please select a gender!");
+            }
+            else if (dayComboBox.Text == "-Day-" || monthComboBox.Text == "-Month-" || yearComboBox.Text == "-Year-")
+            {
+                MessageBox.Show("Please select an option from all combo boxes in your date of birth!");
+            }
+            else if (programComboBox.Text == "-Select Program-")
+            {
+                MessageBox.Show("Please select a program!");
+            }
+            else
+            {
+                string fullName = $"{firstNameText.Text} {middleNameText.Text} {lastNameText.Text}";
+                string gender = maleRadioButton.Checked ? "Male" : "Female";
+                string birthDate = $"{dayComboBox.Text}/{monthComboBox.Text}/{yearComboBox.Text}";
+                string program = programComboBox.Text;
+                MessageBox.Show($"Student Name: {fullName}\nGender: {gender}\nDate of birth: {birthDate}\nProgram: {program}");
+            }
         }
     }
 }
