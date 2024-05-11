9449a348b52e1123dc7e2e59d19b49589510117d
dragunwf <plarisanmarc2@gmail.com>
2024-05-11 18:22:57 +0800

    Finish Quiz

---
+++ Integrative Programming/Prefinals/Quiz/09_Quiz_1.cs
@@ -0,0 +1,44 @@
+﻿/*
+Note, this program isn't meant to be ran. I'm just here to code the instructions asked.
+1. Create a method named getCircleDiameter that takes a radius of double type as the parameter. The method should return the diameter of a circle. To compute the diameter of a circle, multiply the radius by 2.
+2. Create a struct named Product where its fields are code, description, and price. 
+3. Create a class named Person with an instance variable named full_name. Declare one (1) constructor without a parameter and initialize the variable with a default value.
+4. Create a class named Account with an instance variable named account_number. Make this class a member of the Accounts namespace. 
+*/
+
+// 1.
+class Circle
+{
+    double getCircleDiameter(double radius)
+    {
+        return radius * 2;
+    }
+}
+
+// 2.
+struct Product
+{
+    string code;
+    string description;
+    double price;
+}
+
+// 3.
+class Person
+{
+    string full_name;
+
+    public Person(string full_name)
+    {
+        this.full_name = full_name;
+    }
+}
+
+// 4.
+namespace Accounts
+{
+    class Account
+    {
+        int account_number;
+    }
+}
