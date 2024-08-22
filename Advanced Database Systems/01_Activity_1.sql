e812fd3d89436008a1631a0659879ad5bbf7776d
DragunDev <plarisanmarc@gmail.com>
2024-08-22 15:29:17 +0800

    Finish 01_Activity_1.sql

---
+++ Advanced Database Systems/01_Activity_1.sql
@@ -0,0 +1,9 @@
+-- Declare a variable named LastName.
+-- Set the value of the variable according to your last name.
+-- Print/display the value of variable.
+
+BEGIN
+	DECLARE @LastName AS VARCHAR(50) -- Answer to A.
+	SET @LastName = 'Dragun' -- Answer to B.
+	PRINT @LastName -- Answer to C.
+END
