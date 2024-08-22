321d6274cff70b2ddd90de793bd5013214184cad
DragunDev <plarisanmarc@gmail.com>
2024-08-22 15:54:03 +0800

    Create insert_records_data.sql

---
+++ Advanced Database Systems/01 Laboratory Exercise 1/insert_records_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Records
+VALUES (1, 'Santos', 'Mark Anthony', 17, 'Male', 'Freshman'),
+	   (2, 'Nonat', 'Jayson', 15, 'Male', 'Sophomore'),
+	   (3, 'Valencia', 'Nicole', 20, 'Female', 'Senior'),
+	   (4, 'Campos', 'Jane', 19, 'Female', 'Junior'),
+	   (5, 'Morales', 'Micah', 21, 'Female', 'Senior'),
+	   (6, 'Atienza', 'Eldibert', 17, 'Male', 'Sophomore'),
+	   (7, 'Dela Cruz', 'Philip', 16, 'Male', 'Freshman'),
+	   (8, 'Ramos', 'Loisa', 21, 'Female', 'Senior'),
+	   (9, 'Galang', 'Orlean', 18, 'Male', 'Junior'),
+	   (10, 'Detera', 'Chin', 16, 'Female', 'Freshman')
