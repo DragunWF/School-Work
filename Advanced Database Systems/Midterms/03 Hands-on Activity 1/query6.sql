f3cfbdfeaee3fb5638ca9351cac700fa1e5d8fb3
DragunDev <plarisanmarc@gmail.com>
2024-10-01 19:53:17 +0800

    Update query6.sql

--- Advanced Database Systems/Midterms/03 Hands-on Activity 1/query6.sql
+++ Advanced Database Systems/Midterms/03 Hands-on Activity 1/query6.sql
@@ -1,8 +1,28 @@
 BEGIN
     BEGIN TRANSACTION
     DECLARE @SlarkID AS INT = 50;
+    -- The instructions assumes that Slark is paying on this date
     DECLARE @DateSlarkPaid AS DATE = '2020-08-5';
 
-    
+    -- Tuition applies the 15% off when paying on August 5, 2020
+    IF @DateSlarkPaid = '2020-08-5'
+    BEGIN
+        UPDATE Students
+        SET Balance -= Balance * 0.15, 
+            TuitionFee = TuitionFee * 0.15
+        WHERE S_ID = @SlarkID
+    END
+
+    -- Slark decides to make a full payment with the discount applied
+    DECLARE @SlarkPayment AS INT = (
+        SELECT Balance FROM Students WHERE S_ID = @SlarkPayment
+    )
+    UPDATE Students
+    SET Balance -= @SlarkPayment
+    FROM S_ID = @SlarkPayment
+
+    INSERT INTO Payments
+    VALUES (6, @DateSlarkPaid, @SlarkPayment, @SlarkID)
+
     COMMIT
 END
