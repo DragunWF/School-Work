6363fd1ed3977c6bc14ee35ee10eca47107d0585
DragunDev <plarisanmarc@gmail.com>
2024-10-01 19:20:37 +0800

    Create query3-concise.sql

---
+++ Advanced Database Systems/Midterms/03 Hands-on Activity 1/query3-concise.sql
@@ -0,0 +1,15 @@
+BEGIN
+	BEGIN TRANSACTION
+    
+    DECLARE @PaymentAmount AS INT = 1000;
+    
+    UPDATE Students
+    SET Balance -= @PaymentAmount
+    WHERE S_ID = 70 OR S_ID = 20; -- Medusa and Leshrac IDs
+    
+    INSERT INTO Payment
+    VALUES (2, '2020-07-21', @PaymentAmount, 20),
+    	   (3, '2020-07-21', @PaymentAmount, 70);
+                                      
+    COMMIT
+END
