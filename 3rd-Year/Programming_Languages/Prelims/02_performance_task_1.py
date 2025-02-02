3f8dae8b55c78cd209441c48484a49d27a457a42
DragunDev <plarisanmarc@gmail.com>
2025-02-02 22:40:29 +0800

    Create and finish 02_performance_task_1.py

---
+++ 3rd-Year/Programming_Languages/Prelims/02_performance_task_1.py
@@ -0,0 +1,29 @@
+# ======== 2nd Instruction ========
+print("I walk this empty street,")
+print()
+print("On the Boulevard of Broken Dreams.")
+
+# ======== 3rd Instruction ========
+print("I walk this empty street,")
+print("\n")
+print("On the Boulevard of Broken Dreams.")
+
+# ======== 4th Instruction ========
+print("I walk", "this", "empty street")
+print("\n")
+print("On the Boulevard of Broken Dreams.")
+
+# ======== 5th Instruction ========
+print("I walk", "this", "empty street", end=" ")
+print("\n")
+print("On the Boulevard of Broken Dreams.")
+
+# ======== 6th Instruction ========
+print("I walk", "this", "empty street", end=" ")
+print("\n")
+print("On the", "Boulevard of", "Broken Dreams.", sep="-")
+
+# ======== 7th Instruction ========
+print("I walk", "this", "empty street", end=" ", sep="-")
+print("\n")
+print("On the", "Boulevard of", "Broken Dreams.", sep="-", end=" ")
