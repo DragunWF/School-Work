7718a864381b9d1300f376d0ed63dc30f701c48f
DragunDev <plarisanmarc@gmail.com>
2025-02-02 21:39:48 +0800

    Create and finish 02_lab_exercise_1.py

---
+++ 3rd-Year/Programming_Languages/Prelims/02_lab_exercise_1.py
@@ -0,0 +1,10 @@
+# ======== First Instruction ========
+print("Hello, World!")
+
+# ======== Second Instruction ========
+print("Marc")
+
+# ======== Instructions on Simulating Errors ========
+# print(Marc)
+# print "Marc"
+# print(5 / 0)
