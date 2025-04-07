77bdf535e42b7f01a976fc3edefbb4ab88e7e283
DragunWF <plarisanmarc2@gmail.com>
2025-04-07 10:41:42 +0800

    Finish activity for web tech

---
+++ 3rd-Year/Web_Systems_and_Technologies/Prefinals/activity_functions.js
@@ -0,0 +1,24 @@
+// 06 ELMS Activity 1 - Marc Plarisan | BSIT601
+
+// 1.
+function printName(name) {
+  console.log(`My name is ${name}.`);
+}
+
+// 2.
+function addTwoNumbers(firstNumber, secondNumber) {
+  const sum = firstNumber + secondNumber;
+  console.log(sum);
+  return sum;
+}
+
+// 3.
+function addThreeNumbers(firstNumber, secondNumber, thirdNumber) {
+  const sum = firstNumber + secondNumber + thirdNumber;
+  console.log(sum);
+  return sum;
+}
+
+printName("Marc Plarisan");
+addTwoNumbers(5, 3);
+addThreeNumbers(12, 13, 17);
