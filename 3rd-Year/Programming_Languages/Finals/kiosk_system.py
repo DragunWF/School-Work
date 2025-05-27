ddbf21915f3a7c85780099d7a836cadbd372401d
DragunDev <plarisanmarc@gmail.com>
2025-05-27 17:43:16 +0800

    Checkpoint on kiosk_system.py

---
+++ 3rd-Year/Programming_Languages/Finals/kiosk_system.py
@@ -0,0 +1,24 @@
+# Marc Plarisan | BSIT601 | Programming Languages
+# Finals - 08 Laboratory Exercise 1
+
+
+class System:
+    pass
+
+
+class Program:
+    @staticmethod
+    def main() -> None:
+        pass
+
+
+def session_counter() -> None:
+    pass
+
+
+def kiosk_usage() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    Program.main()
