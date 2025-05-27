dbb923121471a5c05c9970daf20bf97dd4dab629
DragunDev <plarisanmarc@gmail.com>
2025-05-27 19:26:34 +0800

    Finish employee and movie list web app

---
+++ 3rd-Year/Web_Systems_and_Technologies/Finals/PT_Plarisan_Marc/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PT_Plarisan_Marc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
