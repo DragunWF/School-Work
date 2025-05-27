385fc4134cc0a76f75a1c9d113bc2d5c34cbd2cb
DragunDev <plarisanmarc@gmail.com>
2025-05-27 09:56:55 +0800

    Create ConsoleApp activity

---
+++ 3rd-Year/Web_Systems_and_Technologies/Finals/PlarisanMarcConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PlarisanMarcConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Marc Plarisan");
+        }
+    }
+}
