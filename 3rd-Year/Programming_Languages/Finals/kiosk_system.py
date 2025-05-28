81d31ad58652a87ffb60132630d1fe20f1b8a389
DragunDev <plarisanmarc@gmail.com>
2025-05-28 18:41:52 +0800

    Finish kiosk_system.py activity on Programming Languages subject

--- 3rd-Year/Programming_Languages/Finals/kiosk_system.py
+++ 3rd-Year/Programming_Languages/Finals/kiosk_system.py
@@ -1,24 +1,35 @@
 # Marc Plarisan | BSIT601 | Programming Languages
-# Finals - 08 Laboratory Exercise 1
+# Finals - 08 Performance Task 1
 
+def main() -> None:
+    # Defined according to the instructions
+    CUSTOMER_SESSION_COUNT = 3
+    SESSION_COUNTER_DEMONSTRATION_COUNT = 5
 
-class System:
-    pass
+    # Instruction #3 (Calling both functions three times)
+    print("===== Customer Sessions ===== ")
+    for i in range(CUSTOMER_SESSION_COUNT):
+        session_counter()
+        kiosk_usage()
 
-
-class Program:
-    @staticmethod
-    def main() -> None:
-        pass
+    # Instruction #4 - Calling session_counter() five times to show it resets
+    print("\n===== Session Counter Demonstration ===== ")
+    for i in range(SESSION_COUNTER_DEMONSTRATION_COUNT):
+        session_counter()
 
 
 def session_counter() -> None:
-    pass
+    counter = 0
+    counter += 1
+    print(f"Session visits: {counter}")
 
 
 def kiosk_usage() -> None:
-    pass
+    if not hasattr(kiosk_usage, "total_users"):
+        kiosk_usage.total_users = 0
+    kiosk_usage.total_users += 1
+    print(f"Total users today: {kiosk_usage.total_users}")
 
 
 if __name__ == "__main__":
-    Program.main()
+    main()
